7838c9f822695f5340e7b31740c7f296afe9c942
AlfredoVR <alfredo2.villarroya@gmail.com>
2024-01-19 14:32:31 +0000

    remove path and indicated in Main where to write it

--- src/main/java/software/ulpgc/imageviewerMVC/Main.java
+++ src/main/java/software/ulpgc/imageviewerMVC/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.io.File;
 
 public class Main {
-    public static final String root = "path";
+    public static final String root = "path ";
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
